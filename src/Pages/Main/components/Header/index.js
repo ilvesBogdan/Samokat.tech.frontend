d230edd05e5f14f626fdc575e37758dab6326472
Bogdan <dzhumabaev.bogdan@ya.ru>
2023-05-13 13:42:38 +0700

    Создание проекта

---
+++ src/Pages/Main/components/Header/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+const Footer = ()=> {
+    return (
+        <footer className='footer'>
+            <span className='footer__logo'>САМОКАТ</span>
+            <a href='#'>Новости</a>
+            <a href='#'>Личный кабинет</a>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/Pages/Main/components/Search/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Search = () => {
+    return (
+        <main>
+            <form>
+                <input type="text" />
+                <input type="submit" value="Отправить" />
+            </form>
+        </main>
+    );
+}
+
+export default Search;
