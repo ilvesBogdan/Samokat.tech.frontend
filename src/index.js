543bac3e8d7f92f81e86e91b88690aa450e85e01
Bogdan <dzhumabaev.bogdan@ya.ru>
2023-05-14 09:00:31 +0700

    rm normalize.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'normalize.css'
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Main from "./Pages/Main";
