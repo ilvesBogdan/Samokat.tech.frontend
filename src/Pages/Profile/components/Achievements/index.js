c655bcbfd73c791204263f5a075d0e04bef812ec
Bogdan <dzhumabaev.bogdan@ya.ru>
2023-05-14 14:29:59 +0700

    Что то сделанно

--- src/Pages/Profile/components/Achievements/index.js
+++ src/Pages/Profile/components/Achievements/index.js
@@ -1,8 +1,24 @@
+import medal from './static/1.svg';
+import graph from './static/2.svg';
+import logsa from './static/3.png';
+
 import './index.css';
 
 const Achievements = () => {
     return (
-        <div className='profile-block profile-border profile__achievements-block'></div>
+        <div className='profile-block profile-border profile__achievements-block'>
+            <div>
+            <div><span>Награды</span>
+                <img src={medal} />
+            </div>
+            <div><span>Рейтинг</span>
+                <img src={graph}/>
+            </div>
+            <div><span>Баллы</span>
+                <img src={logsa}/>
+            </div>
+            </div>
+        </div>
     );
 }
 
--- src/Pages/Profile/components/Comment/index.js
+++ src/Pages/Profile/components/Comment/index.js
@@ -2,7 +2,20 @@ import './index.css';
 
 const Comment = () => {
     return (
-        <div className='profile-block profile-border profile__comment-block'></div>
+        <div className='profile-block profile-border profile__comment-block'>
+            <h3>Оставить Комплимент Олегу</h3>
+            <span>Олег, спасибо тебе за то, что</span>
+            <div className='profile__comment-block-flcol'>
+                <div className='profile__comment-block-flrow'>
+                    <div>Рабочий процесс</div>
+                    <div>Общение</div>
+                </div>
+                <div className='profile__comment-block-flrow'>
+                    <div>Личные качества</div>
+                    <div>Достижения</div>
+                </div>
+            </div>
+        </div>
     );
 }
 
