d70c60937d6d03bcec46f5519128811154c92a4f
Bogdan <dzhumabaev.bogdan@ya.ru>
2023-05-14 14:38:24 +0700

    add promt to profile page

--- src/Pages/Profile/components/ProfileInfo/index.js
+++ src/Pages/Profile/components/ProfileInfo/index.js
@@ -1,16 +1,17 @@
 import fotoDog from '../../static/foto.jpg';
 import './index.css';
 
-const ProfileInfo = () => {
+const ProfileInfo = (promt) => {
+    const photoUrl = 'https://company.caaat.pro/upload/avatars/' + promt.photo
     return (
         <>
             <picture className='profile-info profile-block profile-border profile-info__foto'>
-                <img src={fotoDog} alt='Фото профиля' />
+                <img src={photoUrl} alt='Фото профиля' />
             </picture>
             <div className='profile-info__bloc-parent'>
             <div className='profile-info profile-block profile-border profile-info__block'>
                 <div>
-                <h2>Имя фамилия</h2>
+                <h2>{promt.name}</h2>
                 </div>
             </div>
             </div>
--- src/Pages/Profile/index.js
+++ src/Pages/Profile/index.js
@@ -5,12 +5,14 @@ import Achievements from "./components/Achievements";
 import Statistics from "./components/Statistics";
 import './index.css';
 
-const Profile = () => {
+const Profile = (promt) => {
+    const value = promt.value;
+    const name = [value.name, value.surname].join(' ');
     return (
         <>
             <Header/>
             <main className='profile'>
-                <section className='profile__info'><ProfileInfo/></section>
+                <section className='profile__info'><ProfileInfo name={name} photo={value.avatar}/></section>
                 <section className='profile__comment'><Comment/></section>
                 <section className='profile__achievements'><Achievements/></section>
                 <section className='profile__statistics'><Statistics/></section>
