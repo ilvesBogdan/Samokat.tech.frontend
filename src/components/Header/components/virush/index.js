642ba8b17f04563dcf777102690c224a094ad705
Bogdan <dzhumabaev.bogdan@ya.ru>
2023-05-14 18:23:44 +0700

    Выручайка

---
+++ src/components/Header/components/virush/index.js
@@ -0,0 +1,8 @@
+import './index.css';
+const Virush = () => {
+    return (
+        <div className='circle' />
+    );
+}
+
+export default Virush;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import './index.css'
 import logoPng from './static/logo.png';
 import logoWebP from './static/logo.webp';
+import Virush from './components/virush';
 
 const Header = () => {
     return (
@@ -10,6 +11,7 @@ const Header = () => {
                 <source srcSet={logoWebP} type='image/webp' />
                 <img src={logoPng} alt='САМОКАТ' />
             </picture></a>
+            <Virush />
             <nav>
                 <a href='/news'>Новости</a>
                 <a href='/lk'>Личный кабинет</a>
